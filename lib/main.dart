e03057b31dcd520ccfe9be9fbdf8df2f56a35fed
trinagle1289 <denny95090@gmail.com>
2023-10-31 17:36:02 +0800

    add: 調整主要程式檔案
    調整參數的位置
    修改 comsumerWidgit 元件的使用
    新增取得身體姿勢評級、工作條件評級以及風險值的方法

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'ui/title.dart';
 import 'ui/lhc/step1_1.dart';
@@ -15,22 +17,41 @@ import 'ui/lhc/step6_3.dart';
 import 'ui/lhc/step7.dart';
 import 'ui/lhc/step8.dart';
 
-/// 姿勢評級
-var step1of1Data = List<String>.filled(2, ""); // 開始結束姿勢標籤(2 個字串)
-double step1of2Data = 0.0; // 姿態評級
-var step2Data = List<double>.filled(4, 0.0); // 身體姿勢 額外加分的部分(總共 4 類，最多計算 6 分)
-double step3Data = 0.0; // 時間評級
-double step4Data = 0.0; // 負重評級
-String step4GenderData = "男"; // 負重評級性別
-double step5Data = 0.0; // 力量傳遞/負重條件
-// 不良工作條件
-var step6fo1Data = List<double>.filled(2, 0.0); // [搬運/握持受限, 空間條件]
-var step6fo2Data = List<double>.filled(2, 0.0); // [手/手臂的位置與動作, 氣候條件]
-var step6fo3Data = List<double>.filled(2, 0.0); // [力量傳遞/應用受限, 衣服條件]
-double step7Data = 0.0; // 工作協調/時間分佈
-
 /// 主程式
-void main() => const TitleApp();
+void main() => runApp(const TitleApp());
+
+/// 姿勢評級的開始結束姿勢標籤(2 個字串)
+var step1of1Data = List<String>.filled(2, "");
+
+/// 姿態評級
+double step1of2Data = 0.0;
+
+/// 身體姿勢 額外加分的部分(總共 4 類，最多計算 6 分)
+var step2Data = List<double>.filled(4, 0.0);
+
+/// 時間評級
+double step3Data = 0.0;
+
+/// 負重評級
+double step4Data = 0.0;
+
+/// 負重評級性別
+String step4GenderData = "男";
+
+/// 力量傳遞/負重條件
+double step5Data = 0.0;
+
+/// 不良工作條件[搬運/握持受限, 空間條件]
+var step6fo1Data = List<double>.filled(2, 0.0);
+
+/// 不良工作條件[手/手臂的位置與動作, 氣候條件]
+var step6fo2Data = List<double>.filled(2, 0.0);
+
+/// 不良工作條件[力量傳遞/應用受限, 衣服條件]
+var step6fo3Data = List<double>.filled(2, 0.0);
+
+/// 工作協調/時間分佈
+double step7Data = 0.0;
 
 /// 取得 LHC 介面
 Widget getLhcApp(String idx) {
@@ -38,13 +59,13 @@ Widget getLhcApp(String idx) {
 
   switch (idx) {
     case "1-1":
-      widget = Step1of1App();
+      widget = ProviderScope(child: Step1of1App());
       break;
     case "1-1-Load":
       widget = Step1of1LoadingApp();
       break;
     case "1-2":
-      widget = Step1of2App();
+      widget = ProviderScope(child: Step1of2App());
       break;
     case "1-3":
       widget = const Step1of3App();
@@ -73,9 +94,9 @@ Widget getLhcApp(String idx) {
     case "7":
       widget = const Step7App();
       break;
-    // case "8":
-    //   widget = const Step8App();
-    //   break;
+    case "8":
+      widget = ProviderScope(child: Step8App());
+      break;
     default:
       widget = const TitleApp();
       break;
@@ -92,11 +113,42 @@ AppBar getTitleAppBar(String title) => AppBar(
     flexibleSpace: Container(color: Colors.blue),
     title: Text(title, style: const TextStyle(fontSize: 40)));
 
-/// 重置資訊
-void reset() {
-  step1of1Data = List<String>.filled(2, ""); // 開始結束姿勢標籤(2 個字串)
-  step1of2Data = 0.0; // 姿態評級
-  step2Data = List<double>.filled(4, 0.0); // 身體姿勢 額外加分(總共 4 類，最多計算 6 分)
+/// 取得身體姿勢評級
+double getPoseData() {
+  // 身體姿勢額外加分(最多 6 分)
+  var bodyExtraData =
+      (step2Data[0] + step2Data[1] + step2Data[2] + step2Data[3]) < 6
+          ? (step2Data[0] + step2Data[1] + step2Data[2] + step2Data[3])
+          : 6.0;
+  // 身體姿勢
+  var bodyData = step1of2Data + bodyExtraData;
+  return bodyData;
+}
+
+/// 取得工作條件評級
+double getWorkingConditionsData() {
+  return step6fo1Data[0] +
+      step6fo1Data[1] +
+      step6fo2Data[0] +
+      step6fo2Data[1] +
+      step6fo3Data[0] +
+      step6fo3Data[1];
+}
+
+/// 取得風險值
+double getRiskLevel() =>
+    step3Data *
+    (step4Data +
+        step5Data +
+        getPoseData() +
+        getWorkingConditionsData() +
+        step7Data);
+
+/// 重置全部資訊
+void resetAllData() {
+  step1of1Data = List<String>.filled(2, ""); // 姿勢評級的開始結束姿勢標籤(2 個字串)
+  step1of2Data = 0.0; // 姿勢評級
+  step2Data = List<double>.filled(4, 0.0); // 身體姿勢 額外加分的部分(總共 4 類，最多計算 6 分)
   step3Data = 0.0; // 時間評級
   step4Data = 0.0; // 負重評級
   step4GenderData = "男"; // 負重評級性別
